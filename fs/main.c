efcf6927f1d849c11c3476faf6d5da6a22a5e10b
sj2903 <sj2903@mail.ru>
2015-01-15 01:03:04 +0300

    creation funcion implemented

--- fs/main.c
+++ fs/main.c
@@ -2,7 +2,8 @@
 
 #include <stdio.h>         
 #include <stdlib.h>         
-#include <string.h>        
+#include <string.h>  
+
 #include <errno.h>         
 #include <fcntl.h>  /* manipulate file descriptor */
 #include <sys/types.h>  /* mode_t */
@@ -16,22 +17,25 @@
 #define MAX_FPATH_LENGTH 500
 
 typedef char content_t;
+typedef enum  {false, true} bool;
 
 typedef struct {
-    char * path;
+    char  path[500];
     mode_t mode; 
     content_t* content;
     long nlinks;
     long size;
-    int isFree;
+    int isUsed;
 } fileInfo_t;
 
-static char *fs_str;
-static char *fs_path;
-static mode_t   mode;
+char *fs_str;
+char *fs_path;
+mode_t   mode;
 
+static fileInfo_t inodes[MAX_FILES_COUNT];
 
 static int fs_getattr(const char *path, struct stat *stbuf);
+static int fs_mknod(const char *path, mode_t mode, dev_t rdev);
 static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
              off_t offset, struct fuse_file_info *fi);
 static int fs_open(const char *path, struct fuse_file_info *fi);
@@ -47,8 +51,20 @@ static int fs_access(const char *path, int functMode);
 static int fs_write(const char *path, const char *buf, size_t size,
              off_t offset, struct fuse_file_info *fi);
 
+int findFileIndex(const char * path);
+void sendNotification(const char * name, const char * text);
+long getUnusedNodeIndex();
 
-
+void initNode(long index, const char *path, const char * content, mode_t mode)
+{
+    inodes[index].mode = mode;
+    strcpy(inodes[index].path, path);
+    inodes[index].content = (content_t *) malloc(500);
+    strcpy(inodes[index].content, content);
+    inodes[index].nlinks = 1;
+    inodes[index].size = strlen(inodes[index].content);
+    inodes[index].isUsed = 1;    
+}
 
 static struct fuse_operations fs_operations = {
     .getattr    = fs_getattr,
@@ -57,6 +73,7 @@ static struct fuse_operations fs_operations = {
     .open       = fs_open,
     .read       = fs_read,
     .write      = fs_write,
+    .mknod      = fs_mknod,
 
     .access     = fs_access,
 
@@ -67,36 +84,86 @@ static struct fuse_operations fs_operations = {
 
 int main(int argc, char *argv[])
 {
+    memset(inodes, 0, sizeof(fileInfo_t) * MAX_FILES_COUNT);
 
-    fs_str = (char*) malloc(500);
-    fs_path = (char*) malloc(500);
-    strcpy(fs_str, "Hello World!\n");
-    strcpy(fs_path, "/hello");
-    mode = S_IFREG | 0644;
+    inodes[0].mode = S_IFREG | 0644;
+    strcpy(inodes[0].path, "/hello");
+    inodes[0].content = (content_t *) malloc(500);
+    strcpy(inodes[0].content, "Hello World!\n");
+    inodes[0].nlinks = 1;
+    inodes[0].size = strlen(inodes[0].content);
+    inodes[0].isUsed = 1;
+
+    sendNotification("Warning", "asd");
 
     fuse_main(argc, argv, &fs_operations, NULL); 
-    free(fs_path);
-    free(fs_str);
+
+
     return 0;
-    //return fuse_main(argc, argv, &fs_oper, NULL);
 }
 
-int fs_write(const char *path, const char *buf, size_t size,
-             off_t offset, struct fuse_file_info *fi)
+static int fs_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+    int res;
+    int index;
+
+    if (S_ISREG(mode)) {
+        if((index = getUnusedNodeIndex())== -1) return -ENOMEM;
+        initNode(index, path, "\0", mode);
+
+    } else if (S_ISFIFO(mode)) {
+        sendNotification("Can't create named pipe", "(function is not implemented)");
+        res = -1;
+    } else {
+        sendNotification("Can't create device special file", "(function is not implemented)");
+        res = -1;
+    }
+        
+
+    return 0;
+}
+
+int fs_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
-    (void) fi;
-    strcpy(fs_str + offset, buf);
+    int index;
+
+    memset(stbuf, 0, sizeof(struct stat));
+    if (strcmp(path, "/") == 0) {
+        stbuf->st_mode = S_IFDIR | 0755;
+        stbuf->st_nlink = 2;
+    } else if((index = findFileIndex(path)) != -1) {
+        stbuf->st_mode = inodes[index].mode;
+        stbuf->st_nlink = inodes[index].nlinks;
+        stbuf->st_size = inodes[index].size;
+    } else {
+        res = -ENOENT;
+    }
+
     return res;
 }
 
 
 
+int fs_write(const char *path, const char *buf, size_t size,
+             off_t offset, struct fuse_file_info *fi)
+{
+    int res = 0;
+    (void) fi;  
+    int index;
+    if((index = findFileIndex(path)) == -1)
+        return -ENOENT;
 
+    printf("(%s %d)\n\n\n\n\n\n", inodes[index].content, size);
+    strcpy(inodes[index].content + offset, buf);
+    inodes[index].size = size;
+    return res;
+}
 
 int fs_access(const char *path, int functMode)
 {
-    int res;
+    //int res;
+
     switch(functMode) {
         case R_OK:
             break;
@@ -115,46 +182,28 @@ int fs_access(const char *path, int functMode)
 
 int fs_open(const char *path, struct fuse_file_info *fi)
 {
-    if (strcmp(path, fs_path) != 0)
+    if (findFileIndex(path) == -1)
         return -ENOENT;
-
-    //if ((fi->flags & 3) != O_RDONLY)
-    //    return -EACCES;
 
     return 0;
 }
 
-int fs_getattr(const char *path, struct stat *stbuf)
-{
-    int res = 0;
 
-    memset(stbuf, 0, sizeof(struct stat));
-    if (strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    } else if (strcmp(path, fs_path) == 0) {
-        stbuf->st_mode = mode;
-        stbuf->st_nlink = 1;
-        stbuf->st_size = strlen(fs_str);
-    } else
-        res = -ENOENT;
-
-    return res;
-}
 
 int fs_read(const char *path, char *buf, size_t size, off_t offset,
               struct fuse_file_info *fi)
 {
     size_t len;
     (void) fi;
-    if(strcmp(path, fs_path) != 0)
+    int index;
+    if((index = findFileIndex(path)) == -1)
         return -ENOENT;
 
-    len = strlen(fs_str);
+    len = strlen(inodes[index].content);
     if (offset < len) {
         if (offset + size > len)
             size = len - offset;
-        memcpy(buf, fs_str + offset, size);
+        memcpy(buf, inodes[index].content + offset, size);
     } else
         size = 0;
 
@@ -166,13 +215,17 @@ int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 {
     (void) offset;
     (void) fi;
+    int i;
 
     if (strcmp(path, "/") != 0)
         return -ENOENT;
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
-    filler(buf, fs_path + 1, NULL, 0);
+
+    for(i = 0; i < MAX_FILES_COUNT; i++) {
+        if(strlen(inodes[i].path) != 0) filler(buf, inodes[i].path + 1, NULL, 0);
+    }
 
     return 0;
 }
@@ -180,19 +233,58 @@ int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 int fs_rename(const char *from, const char *to)
 {
-    strcpy(fs_path, to);
+    int index;
+    if((index = findFileIndex(from)) == -1)
+        return -ENOENT;
+
+    strcpy(inodes[index].path, to);
     return 0;
 }
 
 int fs_chmod(const char *path, mode_t newMode)
 {
-    mode = newMode;
+    int index;
+    if((index = findFileIndex(path)) == -1)
+        return -ENOENT;
+
+    inodes[index].mode = newMode;
     return 0;
 }
 
 int fs_truncate(const char *path, off_t size)
 {
-    fs_str[0] = '\0';
-    fprintf(stderr, "\n\n\n\n\n\n\n\n\n");
+    int index;
+    if((index = findFileIndex(path)) == -1)
+        return -ENOENT;
+    inodes[index].content[0] = '\0';
+    inodes[index].size = 0;
     return 0;
+}
+
+int findFileIndex(const char * path)
+{
+    int i;
+    for(i = 0; i < MAX_FILES_COUNT; i++) {
+        if(inodes[i].path && ! strncmp(inodes[i].path, path, MAX_FPATH_LENGTH)) return i;
+    }
+    return -1;
+}
+
+void sendNotification(const char * name, const char * text)
+{
+    static char notifyIcon[] = "dialog-information";
+    notify_init (name);
+    NotifyNotification * notif = notify_notification_new (name, text, notifyIcon);
+    notify_notification_show (notif, NULL);
+    g_object_unref(G_OBJECT(notif));
+    notify_uninit();
+}
+
+long getUnusedNodeIndex()
+{
+    int i;
+    for(i = 0; i < MAX_FILES_COUNT; i++) {
+        if(! inodes[i].isUsed) return i;
+    }
+    return -1;
 }
