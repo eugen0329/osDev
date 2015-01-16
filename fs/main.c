36a29a2634bbce33525ed5b9715c4835398ed5ca
sj2903 <sj2903@mail.ru>
2015-01-16 04:54:17 +0300

    implemented rename function

--- fs/main.c
+++ fs/main.c
@@ -2,6 +2,7 @@
 #pragma GCC diagnostic ignored "-Wunused-function"
 
 
+
 #include <stdio.h>         
 #include <stdlib.h>         
 #include <string.h>  
@@ -11,6 +12,8 @@
 #include <sys/types.h>  /* mode_t */
 #include <unistd.h> /* access funcion mode macro */
 #include <stdint.h>
+#include <libgen.h> /* basename, dirname */
+
 
 #include <libnotify/notify.h>
 #include <fuse.h>          
@@ -23,6 +26,8 @@
 #define FS_SIZE         100000000
 #define MAX_NAME_LEN    255
 #define INODES_VEC sizeof(superblock_t)
+#define DATA_BLOCK_SIZE 1024
+#define DIR_ENTR_IN_BLOCK ((sb->dataBlockSize / sizeof(dirEntry_t)) - 1)
 
 typedef char content_t;
 typedef struct fuse_file_info fuseFInfo_t;
@@ -37,7 +42,6 @@ typedef struct {
     int isUsed;
 } fileInfo_t;
 
-
 typedef struct {
     uint32_t fsSize;
     uint32_t nInodes;
@@ -45,7 +49,7 @@ typedef struct {
 
     uint32_t firstBlock;
 
-    uint32_t blockSize;
+    uint32_t dataBlockSize;
 } superblock_t;
 
 typedef struct {
@@ -60,6 +64,7 @@ typedef struct {
     uint32_t inode;
     char name[MAX_NAME_LEN + 1];
 } dirEntry_t;
+
 
 static fileInfo_t inodes[MAX_FILES_COUNT];
 static void* vdev; 
@@ -81,12 +86,16 @@ int findFileIndex(const char * path);
 void sendNotification(const char * name, const char * text);
 long getUnusedNodeIndex();
 void fs_init();
-inode_t* findInodeFromEntry(void * block, const char* name, uint16_t nameLen);
+dirEntry_t* getDirEntry(void * block, const char* name, uint16_t nameLen);
 int getCharIndex(const char * str, char target);
+int rgetCharIndex(const char * str, char target);
 
 void initNode(long index, const char *path, mode_t mode);
-inode_t* getInode(const char *path);
-inode_t* findInodeFromEntry(void * block, const char* name, uint16_t nameLen);
+inode_t* getInodeByPath(const char *path);
+//dirEntry_t* getDirEntry(void * block, const char* name, uint16_t nameLen);
+
+inode_t* getInode(uint64_t id);
+void* getDataBlock(uint64_t id);
 
 static struct fuse_operations fs_operations = {
     .getattr    = fs_getattr,
@@ -95,16 +104,30 @@ static struct fuse_operations fs_operations = {
     .open       = fs_open,
     .read       = fs_read,
 
-    //.write      = fs_write,
+    .access     = fs_access,
+
+    .write      = fs_write,
 
     //.mknod      = fs_mknod,
     //.unlink     = fs_unlink,
 
-    //.rename     = fs_rename,                           
+    .rename     = fs_rename,                           
     //.chmod      = fs_chmod,                           
     //.truncate   = fs_truncate, 
-    //.access     = fs_access,
+
 };
+
+void test();
+
+/*typedef struct { */
+    /*uint32_t inode;*/
+    /*char name[MAX_NAME_LEN + 1];*/
+/*} dirEntry_t;*/
+
+
+char * getBasename(const char * path);
+char * getDirname(const char * path);
+
 
 int main(int argc, char *argv[])
 {
@@ -119,19 +142,7 @@ int main(int argc, char *argv[])
 
     fs_init();
 
-
-    char * data = (char *) malloc(500);
-    inode_t* inode ;//= getInode("/");
-
-
-    //if((inode = getInode("/.trash")) == NULL) exit(1);
-    //memcpy(data, vdev + inode->blocks[0], inode->size);
-    //data[inode->size] = '\0';
-    //printf("size %d, mode %x, nlinks %d, data \"%s\"\n", inode->size, inode->mode, inode->nLinks, data); 
-    //exit(1); 
-
     fuse_main(argc, argv, &fs_operations, NULL); 
-
     free(vdev);
 
     return 0;
@@ -139,60 +150,15 @@ int main(int argc, char *argv[])
 
 
 
-inode_t* getInode(const char *path)
-{
-    inode_t* inode = (inode_t *)(vdev + INODES_VEC);
-    int nameStart = 1;
-    int nameEnd = 1;
-
-    if(! strcmp(path, "/")) return inode;
-
-    while(1) {
-        if( (nameEnd = getCharIndex(path + nameStart, '/')) == -1) nameEnd = strlen(path);
-        //printf("paht = %s,  name: start = %d, end = %d\n", path, nameStart, nameEnd);
-        
-
-        inode = findInodeFromEntry(vdev + inode->blocks[0], path + nameStart, nameEnd - nameStart);
-        
-        printf("+\n");
-        
-        if(inode == NULL || path[nameEnd] == '\0'){
-            break;
-        } 
-        //printf("getInode:finded sizee%d\n", inode->size);
-        nameStart = nameEnd;        
-    }
-
-    return inode;
-}
-
-inode_t* findInodeFromEntry(void * block, const char* name, uint16_t nameLen)
-{
-    dirEntry_t* de = (dirEntry_t *) block;
-    int i;
-    int lim = (sb->blockSize / sizeof(dirEntry_t)) - 2;
-
-    for(i = 0; i < lim; i++) {
-        printf("de = %s\n", de->name);
-        if(strlen(de->name) == nameLen && ! strncmp(de->name, name, nameLen)) {
-            return (inode_t *) (vdev + de->inode);
-        }
-        de += sizeof(dirEntry_t);
-    }
-    printf("-\n");
-    return NULL;
-}
-
-
 int fs_getattr(const char *path, struct stat *stbuf)
 {
     int res = 0;
-    int index;
+    //int index;
     inode_t * inode;
 
     memset(stbuf, 0, sizeof(struct stat));
    
-    if( (inode = getInode(path)) != NULL) {
+    if( (inode = getInodeByPath(path)) != NULL) {
         stbuf->st_mode = inode->mode;
         stbuf->st_nlink = inode->nLinks;
         stbuf->st_size = inode->size;
@@ -245,15 +211,20 @@ int fs_write(const char *path, const char *buf, size_t size, off_t offset, fuseF
     if((index = findFileIndex(path)) == -1)
         return -ENOENT;
 
-    strcpy(inodes[index].content + offset, buf);
-    inodes[index].size = size;
+
+    inode_t * inode;
+    if((inode = getInodeByPath(path)) == NULL) return -ENOENT;
+    void* dataBlock = getDataBlock(inode->blocks[0]);
+
+    memcpy(dataBlock + offset, buf, strlen(buf));
+    inode->size = size;
+    //strcpy(inodes[index].content + offset, buf);
+    //inodes[index].size = size;
     return res;
 }
 
 int fs_access(const char *path, int functMode)
 {
-    //int res;
-
     switch(functMode) {
         case R_OK:
             break;
@@ -272,9 +243,7 @@ int fs_access(const char *path, int functMode)
 
 int fs_open(const char *path, fuseFInfo_t *fi)
 {
-    if (findFileIndex(path) == -1)
-        return -ENOENT;
-
+    if(getInodeByPath(path) == NULL) return -ENOENT;
     return 0;
 }
 
@@ -282,15 +251,15 @@ int fs_read(const char *path, char *buf, size_t size, off_t offset, fuseFInfo_t 
 {
     size_t len;
     (void) fi;
-    int index;
-    if((index = findFileIndex(path)) == -1)
-        return -ENOENT;
 
-    len = strlen(inodes[index].content);
-    if (offset < len) {
-        if (offset + size > len)
-            size = len - offset;
-        memcpy(buf, inodes[index].content + offset, size);
+    inode_t * inode;
+    if((inode = getInodeByPath(path)) == NULL) return -ENOENT;
+    void* dataBlock = getDataBlock(inode->blocks[0]);
+
+    len = inode->size;
+    if(offset < len) {
+        if (offset + size > len) size = len - offset;
+        memcpy(buf, dataBlock + offset, size);
     } else
         size = 0;
 
@@ -302,16 +271,16 @@ int fs_readdir(const char *path, void *buf, filler_t filler, off_t offset, fuseF
     (void) offset;
     (void) fi;
     int i;
+    int lim = DIR_ENTR_IN_BLOCK;
 
-    if (strcmp(path, "/") != 0) {
-        return -ENOENT;
-    }  
+    inode_t * inode = getInodeByPath(path);
+    dirEntry_t * de = (dirEntry_t *) getDataBlock(inode->blocks[0]);
 
     filler(buf, ".", NULL, 0);
     filler(buf, "..", NULL, 0);
 
-    for(i = 0; i < MAX_FILES_COUNT; i++) {
-        if(strlen(inodes[i].path) != 0) filler(buf, inodes[i].path + 1, NULL, 0);
+    for(i = 0; i < lim; i++) {
+        if(de[i].inode != 0 ) filler(buf, de[i].name, NULL, 0);
     }
 
     return 0;
@@ -324,9 +293,27 @@ int fs_rename(const char *from, const char *to)
     if((index = findFileIndex(from)) == -1)
         return -ENOENT;
 
-    strcpy(inodes[index].path, to);
+    char * dir = getDirname(from);
+    char * oldName = getBasename(from);
+    char * newName = getBasename(to);
+
+    printf("%s %s %s\n", dir, oldName, newName);
+    printf("rename %s %s \n", from, to);
+    inode_t * inode;
+    if((inode = getInodeByPath(dir)) == NULL) return -ENOENT;
+    dirEntry_t * de = getDirEntry(getDataBlock(inode->blocks[0]), oldName, strlen(oldName));
+    if(de == NULL) return -ENOENT;
+    printf("%s\n", de->name);
+    strcpy(de->name, newName);
+    printf("%s\n", de->name);
+
+    free(dir);
+    free(oldName);
+    free(newName);
     return 0;
 }
+
+//- rgetCharIndex(from)
 
 int fs_chmod(const char *path, mode_t newMode)
 {
@@ -393,7 +380,12 @@ int getCharIndex(const char * str, char target)
     return -1;
 }
 
-
+int rgetCharIndex(const char * str, char target)
+{
+    const char *ptr = strrchr(str, target);
+    if(ptr) return ptr - str;
+    return -1;
+}
 
 void fs_init()
 {
@@ -402,33 +394,105 @@ void fs_init()
     sb->fsSize = FS_SIZE;
     sb->nInodes = 50;
     sb->nBlocks = 100;
-    sb->blockSize = 1024;
+    sb->dataBlockSize = DATA_BLOCK_SIZE;
     sb->firstBlock = sizeof(superblock_t) + sizeof(inode_t) * sb->nInodes;
-    printf("superblock size = %d, inode size = %d, memory block size = %d, dirEnt size = %d\n\n\n\n\n", sizeof(superblock_t), sizeof(inode_t), sb->blockSize, sizeof(dirEntry_t));
-
+    //printf("superblock size = %d, inode size = %d, memory block size = %d, dirEnt size = %d\n\n\n\n\n", sizeof(superblock_t), sizeof(inode_t), sb->dataBlockSize, sizeof(dirEntry_t));
 
     inode_t * inode = (inode_t *) (vdev + sizeof(superblock_t));
     inode->mode = S_IFDIR | 0755;
     inode->nLinks = 2;
-    inode->blocks[0] = sb->firstBlock;
-    inode->size = sb->blockSize;
+    inode->blocks[0] = 0;//sb->firstBlock;
+    inode->size = sb->dataBlockSize;
 
-    dirEntry_t* de = (dirEntry_t *) (vdev + inode->blocks[0]);
-    //strcpy(de->name, "/");
-    //de->inode = INODES_VEC;
-//
-    //de += sb->blockSize;
-    de->inode = INODES_VEC + sizeof(inode_t);
+    dirEntry_t* de = (dirEntry_t *) getDataBlock(inode->blocks[0]);
+    de->inode = 1;
     strcpy(de->name, "hello");    
 
-    inode = (inode_t *) (vdev + INODES_VEC + sizeof(inode_t));
+    inode = getInode(1);
     inode->mode = S_IFREG | 0644;
     inode->nLinks = 1;
     char helloStr[] = "Hello world!";
     inode->size = strlen(helloStr);
-    inode->blocks[0] = sb->firstBlock + sb->blockSize; 
-    void* memBlock = (dirEntry_t *) (vdev + inode->blocks[0]);
+    inode->blocks[0] = 1; 
+    void* memBlock = getDataBlock(inode->blocks[0]);
     memcpy(memBlock, helloStr, strlen(helloStr));    
+}
 
+void test()
+{
+    char * data = (char *) malloc(500);
+    inode_t* inode ;//= getInodeByPath("/");
+    if((inode = getInodeByPath("/hello")) == NULL) exit(1);
+    memcpy(data, getDataBlock(1) , inode->size);
+    data[inode->size] = '\0';
+    //printf("size %d, mode %x, nlinks %d, data \"%s\"\n", inode->size, inode->mode, inode->nLinks, data); 
+    exit(1);     
+}
 
+inode_t* getInode(uint64_t id)
+{
+    return (inode_t*)(vdev + sizeof(superblock_t) + id * sizeof(inode_t));
+}
+
+void* getDataBlock(uint64_t id)
+{
+    return vdev + sb->firstBlock + id * sb->dataBlockSize;
+}
+
+inode_t* getInodeByPath(const char *path)
+{
+    inode_t* inode = getInode(0);
+    int nameStart = 1;
+    int nameEnd = 1;
+
+    if(! strcmp(path, "/")) return inode;
+
+    while(1) {
+        if( (nameEnd = getCharIndex(path + nameStart, '/')) == -1) nameEnd = strlen(path);
+        dirEntry_t * de = getDirEntry(getDataBlock(inode->blocks[0]), path + nameStart, nameEnd - nameStart);
+        inode = (de == NULL) ? NULL : getInode(de->inode);
+        if(de == NULL || path[nameEnd] == '\0') break;
+        nameStart = nameEnd;        
+    }
+
+    return inode;
+}
+
+dirEntry_t* getDirEntry(void * block, const char* name, uint16_t nameLen)
+{
+    
+    int i;
+    int lim = (sb->dataBlockSize / sizeof(dirEntry_t)) - 1;
+    dirEntry_t* de = (dirEntry_t *) block;
+
+    for(i = 0; i < lim; i++) {
+        if(strlen(de[i].name) == nameLen && ! strncmp(de[i].name, name, nameLen)) {
+            return de;
+        }
+    }
+    
+    return NULL;
+}
+
+char * getBasename(const char * path)
+{
+    int start = rgetCharIndex(path, '/') + 1;
+    int len = strlen(path) - start + 1;
+
+    if(len == 0) return NULL;
+    char * base = (char *) malloc(len);
+    strncpy(base, path + start, len);
+    base[len - 1] = '\0';
+    return base; 
+}
+
+char * getDirname(const char * path)
+{
+    int len;
+    len  = rgetCharIndex(path, '/') + 2;
+    printf("len%d\n", len);
+    char * dir = (char *) malloc(strlen(path));
+    strcpy(dir, path);
+    dirname(dir);
+    return dir;     
 }
